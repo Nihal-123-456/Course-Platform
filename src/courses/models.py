184cfa55c81ecf422a9a937d0648f0d0a8eaa78b
Nihal-123-456 <knasn1526@example.com>
2025-08-16 10:44:49 +0600

    applied migrations on course model

--- src/courses/models.py
+++ src/courses/models.py
@@ -2,16 +2,20 @@ from django.db import models
 
 # Create your models here.
 class AccessTypes(models.TextChoices):
-    ANYONE = 'anyone', 'Anyone'
-    EMAIL_REQUIRED = 'email_required', 'Email Required'
+    ANYONE = 'any', 'Anyone'
+    EMAIL_REQUIRED = 'email', 'Email Required'
 
 class StatusTypes(models.TextChoices):
     DRAFT = 'draft', 'Draft'
-    COMING_SOON = 'coming', 'Coming Soon'
-    PUBLISHED = 'published', 'Published'
+    COMING_SOON = 'soon', 'Coming Soon'
+    PUBLISHED = 'publish', 'Published'
+
+def image_upload_path(instance, filename):
+    return f"{filename}"
 
 class Course(models.Model):
     title = models.CharField(max_length=144)
     description = models.TextField(null=True, blank=True)
-    access = models.CharField(max_length=12, choices=AccessTypes.choices, default=AccessTypes.ANYONE)
-    status = models.CharField(max_length=12, choices=StatusTypes.choices, default=StatusTypes.DRAFT)
+    image = models.ImageField(upload_to=image_upload_path, null=True, blank=True)
+    access = models.CharField(max_length=7, choices=AccessTypes.choices, default=AccessTypes.EMAIL_REQUIRED)
+    status = models.CharField(max_length=7, choices=StatusTypes.choices, default=StatusTypes.DRAFT)
