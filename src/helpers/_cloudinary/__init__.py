f9db7b960c60207359beca8481084d44a1e2e246
Nihal-123-456 <knasn1526@example.com>
2025-08-17 15:12:41 +0600

    adding to github repo

--- src/helpers/_cloudinary/__init__.py
+++ src/helpers/_cloudinary/__init__.py
@@ -1,4 +1,4 @@
 from .config import cloudinary_init
-from .service import cloudinary_image_processing, generate_slug_id, get_display_name, get_public_id_prefix, cloudinary_video_processing
+from .services import cloudinary_image_processing, generate_slug_id, get_display_name, get_public_id_prefix, cloudinary_video_processing
 
 __all__ = ["cloudinary_init", "cloudinary_image_processing", "generate_slug_id", "get_display_name", "get_public_id_prefix", "cloudinary_video_processing"] 
